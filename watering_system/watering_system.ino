8567886c2b4557d6a52f51b74ee8e5576e67b320
jimijam <dartbond@rambler.ru>
2023-07-24 17:57:44 +0300

    Remove SD-module, add oled display

--- watering_system/watering_system.ino
+++ watering_system/watering_system.ino
@@ -1,18 +1,28 @@
 #include <SPI.h>
-#include <SD.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+// The pins for I2C are defined by the Wire-library. 
+// On an arduino UNO:       A4(SDA), A5(SCL)
+// On an arduino MEGA 2560: 20(SDA), 21(SCL)
+// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 int relayPin = 6;
 int rainPin = A0;
 int trigPin = 2;
 int echoPin = 3;
 
-int misoPin = 12;
-int mosiPin = 11;
-int sckPin = 13;
-int csPin = 4;
-
-int timeout = 43200000; // milliseconds
+int timeout = 60*60*1000; // 60 minutes
 int wateringTime = 3;
+unsigned long lastWatering = 0;
 
 void setup(){
   // Register Pins
@@ -21,47 +31,65 @@ void setup(){
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 
-  // init SD card module
-  if (!SD.begin(csPin)) {
-    return;
+  Serial.begin(9600);  // debug
+  while (!Serial) ; // wait until Arduino Serial Monitor opens
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
   }
 
-  Serial.begin(9600);  // debug
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(2000); // Pause for 2 seconds
+
+  display.clearDisplay();
+
+  // init relay
+  digitalWrite(relayPin, HIGH);
 }
 
 void loop() {
-  // delay 12 hours
   unsigned long startMillis = millis();
-  while (millis() - startMillis < timeout);
   
-  // write sensor data
-  String fileOutput = generateFileName();
   int sensorValue = analogRead(rainPin);
-  bool flag = saveToFile(fileOutput, String(sensorValue));
+  String screen_data = "Last watering: \n" + String(lastWatering) + " hours ago\n" + "Moist value:" + String(sensorValue);
+
+  // check water level
+  bool water_full = checkWaterLevel();
+  if(water_full == false){
+    screen_data = "No water!";
+  }
+
+  showOnDisplay(screen_data);
+  Serial.println(screen_data); // debug
 
   if(isMoist(sensorValue) == false){
-    Serial.println("Ground is dry!"); // debug    
-    if(checkWater() == true){
-      Serial.println("Water full");
+    Serial.println("Ground is dry!"); // debug   
+    if(water_full == true){
+      Serial.println("Start watering"); // debug
       startWatering(wateringTime);
-    }
-    else{
-      Serial.println("No water!"); // debug
-      // TODO
+      lastWatering = millis() / 60*60*1000; // hours
     }
   }
   else{
-    Serial.println("Everything ok"); // debug    
+    Serial.println("Everything ok"); // debug
   }
+
+  while (millis() - startMillis < timeout); // delay timeout
 }
 
 /* This function takes a duration in seconds
  *  and power on water pump connected by relay
  */
 void startWatering(int duration){
-  int millis = duration * 1000;
+  unsigned long timing = millis();
+  int timeout = duration * 1000;
+
 	digitalWrite(relayPin, LOW);
-	delay(millis);
+	while (millis() - timing < timeout);
 	digitalWrite(relayPin, HIGH);
 }
 
@@ -70,7 +98,7 @@ void startWatering(int duration){
  * YL-69 sensor 
  */
 bool isMoist(int sensorValue){
-  int thresholdValue = 750;
+  int thresholdValue = 350;
   return sensorValue < thresholdValue;
 }
 
@@ -78,11 +106,10 @@ bool isMoist(int sensorValue){
  *  and return True/False
  * HC-SR04 sensor 
  */
-bool checkWater(){
-  int borderValue = 2;  
+bool checkWaterLevel(){
+  int borderValue = 11;  
   long echoTime, distance;
 
-  // HC-SR04 example
   digitalWrite(trigPin, LOW);
   delayMicroseconds(5);
 
@@ -93,32 +120,18 @@ bool checkWater(){
   
   echoTime = pulseIn(echoPin, HIGH);
 
-  // Convert time to distance
-  distance = (echoTime / 2) / 29.1;
+  distance = (echoTime / 2) / 29.1; // Convert time to distance
 
-  // TODO Add check > 3000 It value then sensor too close
-  return distance > borderValue;
+  return distance < borderValue;
 }
 
-String generateFileName(){
-  unsigned long day = 86400000;
-  unsigned long daysFromLaunch = millis() / day;
-  String a = String(daysFromLaunch);
-  String fileName = a + ".txt";
-
-  return fileName;
-}
-
-/* This function save string data to file on SD card
- */
-bool saveToFile(String fn, String data){
-  File dataFile = SD.open(fn, FILE_WRITE);
-  if (dataFile) {
-    dataFile.println(data);
-    dataFile.close();
-    
-    return true;
-  }
- 
-  return false;
+/* This function show screen_data on display
+*/
+bool showOnDisplay(String screen_data){
+  display.clearDisplay(); // Clear display buffer
+  display.setTextSize(1); // Draw 2X-scale text
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.println(screen_data);
+  display.display();      // Show initial text
 }
